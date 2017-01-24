f08e1046427f97c923da3bbb0ef5a1ef0b59394b
Alper Keskin <alper.keskin@argedor.com>
2017-01-24 13:11:00 +0300

    Added a simple character controller which restricts the movement to the grid.

---
+++ Assets/ProjectD/Scripts/RPGCharController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPGCharController : MonoBehaviour
+{
+	private Vector3? _target;
+	private bool _running;
+	private Transform _transform;
+	private int tileSize = 32;
+	public float runMultiplier;
+
+	public float baseSpeed;
+
+	public float speed
+	{
+		get
+		{
+			return ((_running) ? runMultiplier : 1f) * baseSpeed * tileSize;
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		_transform = transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_running = Input.GetKey(KeyCode.LeftShift);
+		if (_target.HasValue)
+		{
+			var d = _target.Value - _transform.position;
+			var _dx = (d.x == 0f) ? 0 : Mathf.Sign(d.x);
+			var _dy = (d.y == 0f) ? 0 : Mathf.Sign(d.y);
+
+			if(_dx != 0f && _dy != 0f)
+			{
+				_dx /= Mathf.Sqrt(2);
+				_dy /= Mathf.Sqrt(2);
+			}
+
+			var delta = (speed * Time.deltaTime);
+
+			_dx *= delta;
+			_dy *= delta;
+
+			if((_dx < 0 && _dx + _transform.position.x < _target.Value.x) || (_dx > 0 && _dx + _transform.position.x > _target.Value.x))
+			{
+				_dx = _target.Value.x - _transform.position.x;
+			}
+			if((_dy < 0 && _dy + _transform.position.y < _target.Value.y) || (_dy > 0 && _dy + _transform.position.y > _target.Value.y))
+			{
+				_dy = _target.Value.y - _transform.position.y;
+			}
+
+			if(Vector2.Distance(_transform.position, _target.Value) < 1f)
+			{
+				_transform.position = new Vector3(_target.Value.x, _target.Value.y, _transform.position.z);
+				_target = null;
+			}
+			else
+			{
+				_transform.Translate(_dx, _dy, 0);
+			}
+		}
+		else
+		{
+			var dx = Input.GetAxisRaw("Horizontal");
+			var dy = Input.GetAxisRaw("Vertical");
+
+			if (dx != 0 || dy != 0)
+			{
+				_target = new Vector3(_transform.position.x + tileSize * dx, _transform.position.y + tileSize * dy);
+			}
+		}
+	}
+}
