6da43154726b922429c34278beb157d7e7fdae68
Alper Keskin <alprkskn@gmail.com>
2017-01-27 21:21:28 +0300

    * A simple sprite sorting mechanism is implemented. * Added some placeholder sprites.

---
+++ Assets/ProjectD/Scripts/Overworld/Level/BaseSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSprite : MonoBehaviour
+{
+
+    public int Levitation;
+    private SpriteRenderer _spriteRenderer;
+
+    public SpriteRenderer SpriteRenderer
+    {
+        get
+        {
+            if(_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+
+            return _spriteRenderer;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/ProjectD/Scripts/Overworld/Level/LevelLoader.cs
+++ Assets/ProjectD/Scripts/Overworld/Level/LevelLoader.cs
@@ -9,7 +9,9 @@ public class LevelLoader : MonoBehaviour
     public GameObject[] LevelPrefabs;
     public RPGCharController Player;
 
+
     private Dictionary<string, GameObject> _levels;
+    private List<BaseSprite> _dynamiclevelObjects;
     private TiledMap _currentLevel = null;
     private string _currentLevelName = null;
     private Pathfinder2D _pathfinder;
@@ -19,7 +21,7 @@ public class LevelLoader : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
 
         _levels = new Dictionary<string, GameObject>();
-        foreach(var lvl in LevelPrefabs)
+        foreach (var lvl in LevelPrefabs)
         {
             _levels.Add(lvl.name, lvl);
         }
@@ -37,7 +39,7 @@ public class LevelLoader : MonoBehaviour
 
         string prevLevel = null;
 
-        if(_currentLevel != null)
+        if (_currentLevel != null)
         {
             // Destroy level
             // Move this under the map class as a function if it gets more complex.
@@ -52,6 +54,8 @@ public class LevelLoader : MonoBehaviour
         GridUtils.SetGridTileSize(_currentLevel.TileHeight);
         _currentLevelName = levelName;
         SetWarpPoints(go);
+        SetSpriteObjects(go);
+
         _pathfinder.Create2DMap();
 
         if (isTransition)
@@ -64,11 +68,27 @@ public class LevelLoader : MonoBehaviour
         return true;
     }
 
+    private void SetSpriteObjects(GameObject level)
+    {
+        _dynamiclevelObjects = new List<BaseSprite>();
+
+        foreach (var sprite in FindObjectsOfType<BaseSprite>())
+        {
+            var sr = sprite.SpriteRenderer;
+            sr.sortingOrder = (_currentLevel.MapHeightInPixels / _currentLevel.TileHeight) - (int)(sprite.transform.position.y - sr.bounds.extents.y) / _currentLevel.TileHeight + sprite.Levitation;
+
+            if (!sprite.gameObject.isStatic)
+            {
+                _dynamiclevelObjects.Add(sprite);
+            }
+        }
+    }
+
     private void SetWarpPoints(GameObject level)
     {
         var spawnLayer = level.transform.Find("Spawn");
 
-        foreach(var child in spawnLayer.GetImmediateChildren())
+        foreach (var child in spawnLayer.GetImmediateChildren())
         {
             var wp = child.gameObject.AddComponent<WarpPoint>();
             wp.PlayerDetected += (x) => LoadLevel(x.name, true);
@@ -83,5 +103,16 @@ public class LevelLoader : MonoBehaviour
         var entryTile = entryLayer.GetImmediateChildren().Where(x => x.name == prevLevel).First();
 
         return entryTile.gameObject;
+    }
+
+    void Update()
+    {
+        if (_dynamiclevelObjects != null)
+        {
+            foreach (var obj in _dynamiclevelObjects)
+            {
+                obj.SpriteRenderer.sortingOrder = (int) ((_currentLevel.MapHeightInPixels / _currentLevel.TileHeight) - (obj.transform.position.y - obj.SpriteRenderer.bounds.extents.y) / _currentLevel.TileHeight + obj.Levitation);
+            }
+        }
     }
 }
