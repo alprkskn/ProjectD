3c63771e38b0f83a37ed3a1a5c721db045858e89
Alper Keskin <alprkskn@gmail.com>
2017-02-10 23:09:56 +0300

    * Created the folder and the manager class for the cat picker game.

---
+++ Assets/ProjectD/Scripts/Overworld/Gameplay/CatPicker/CatPicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectD.Overworld
+{
+	public class CatPicker : MonoBehaviour
+	{
+		private Player _player;
+		private List<GameObject> _targetItems;
+		private List<GameEntity> _cats;
+		private Pathfinder2D _pathfinder;
+
+
+		// TODO: initialize players, targetItems, UI, pathfinder. Whatever is needed when
+		// the game transitions into the CatPicker game.
+
+		// This class will be responsible to assign the cats their targets. Track the player/cat interaction.
+		// Also keep track of the target items.
+
+		public void Initialize(GameObject level, Player player, Pathfinder2D pathfinder)
+		{
+		}
+
+		public void QuitGame()
+		{
+
+		}
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
